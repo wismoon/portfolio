0719582a5ad85fa535a894815c3f4573261ba256
wismoon <49384700+wismoon@users.noreply.github.com>
2024-08-07 10:54:50 +0700

    adding bento grid

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from '@/data'
 
 const Grid = () => {
   return (
-    <div>Grid</div>
+    <section id='about'>
+      <BentoGrid>
+        {gridItems.map (({id, title, description, img, className, imgClassName, titleClassName, spareImg}) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
+            />
+          ))}
+      </BentoGrid>
+    </section>
   )
 }
 
