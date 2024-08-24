6e96a8ca95ed8e50abe0920a92eeaa9edfcb9238
wismoon <49384700+wismoon@users.noreply.github.com>
2024-08-24 21:26:08 +0700

    finishing the bento grid and adding new recent RecentProject

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,21 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProject from "@/components/RecentProject";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
+    <main className="relative min-h-screen bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
         <FloatingNav 
           navItems={
             [{ name: 'Home', link: '/', icon: <FaHome/> }]
           }
         />
         <Hero />
+      <div className="max-w-7xl w-full mx-auto sm:px-10 px-5">
         <Grid />
+        <RecentProject />
       </div>
     </main>
   );
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -90,7 +90,6 @@ export const BentoGridItem = ({
                 </div>
                 {id === 6 && (
                     <BackgroundGradientAnimation>
-                        <div className="absolute z-50 flex items-center justify-center text-white font-bold" />
                     </BackgroundGradientAnimation>
                 )}
             
